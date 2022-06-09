938b55c7759d1cbf367171f2440bc42deb967a62
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2022-06-09 16:28:31 +0530

    Create after_install.sh

---
+++ scripts/after_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install libaries
+cd /home/ubuntu/gangup
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
+#python manage.py collectstatic --no-input
+
+# Set permission for all files
+chown -R ubuntu:ubuntu /home/ubuntu
+
+# Restart services
+sudo service supervisor restart
+sudo service nginx restart
