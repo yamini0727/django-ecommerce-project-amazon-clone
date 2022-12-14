25268581fdc53422df128661b0faa99699675c03
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2022-12-14 15:48:14 +0530

    Delete scripts directory

--- scripts/before_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-sudo apt-get update
-#sudo apt-get install -y python3 python-dev python3-pip ffmpeg supervisor nginx 
-pip install --user --upgrade virtualenv
-sudo rm -rf /home/ubuntu/gangup
